ea1e3e6d2e800f59e25d9f4f6d5cefe5afab9df1
Sai Pc <schand31@asu.edu>
2015-10-20 01:45:38 -0700

    Covered loops, switches

---
+++ loops.c
@@ -0,0 +1,14 @@
+//
+// Created by Sai Prashanth Chandramouli on 10/20/15.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int i = 0;
+    while (i < 5) {
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
