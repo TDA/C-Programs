ad280ef50c566398013aa98f6dfc3a8f793d2294
Sai Pc <schand31@asu.edu>
2015-10-20 00:57:13 -0700

    breaking things, and no errors

--- MoreArrays.c
+++ MoreArrays.c
@@ -1,7 +1,7 @@
 //
 // Created by Sai Prashanth Chandramouli on 10/20/15.
 //
-
+#include <stdio.h>
 int main(int argc, char *argv[]) {
     int numbers[4] = {1};
     char name[4] = {'a'};
@@ -19,30 +19,32 @@ int main(int argc, char *argv[]) {
     // setup the numbers
     int i;
     for (i = 0; i < 4; ++i) {
-        numbers[i] = i + 1;
+        numbers[i] = i + 1 + 32;
     }
 
     // setup the name
     name[0] = 'Z';
     name[1] = 'e';
     name[2] = 'd';
-    name[3] = '\0';
+    name[3] = 'A';
+    name[4] = 'a';
 
-    printf("numbers: %d %d %d %d\n",
+    printf("numbers: %c %c %c %c\n",
            numbers[0], numbers[1],
            numbers[2], numbers[3]);
 
-    printf("name each: %c %c %c %c\n",
+    printf("name each: %d %d %d %d %d\n",
            name[0], name[1],
-           name[2], name[3]);
+           name[2], name[3], name[4]);
 
+    // more idiomatic and better way to declare string literals
     char *another = "Zed";
 
     printf("another: %s\n", another);
 
-    printf("another each: %c %c %c %c\n",
+    printf("another each: %d %d %c %c %c\n",
            another[0], another[1],
-           another[2], another[3]);
+           another[2], another[3], another[4]);
 
     return 0;
 }
