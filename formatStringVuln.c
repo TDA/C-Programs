c3fbfd97943a62f7387e2908822871fe3ce57b11
Sai Pc <schand31@asu.edu>
2015-11-19 15:25:35 -0700

    format vuln

---
+++ formatStringVuln.c
@@ -0,0 +1,14 @@
+//
+// Created by Sai Prashanth Chandramouli on 11/19/15.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    char* num;
+    int sai = 100;
+    scanf("%s", &num);
+    printf(num);
+
+    return 0;
+}
